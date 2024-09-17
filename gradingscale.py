dd9d45ac10df90ff52a17e4a8dfbb246073a53ab
Rifatul599 <RifatulIslam112230@gmail.com>
2024-09-18 02:37:42 +0600

    A student's marks and display their grade according to a grading  scale

---
+++ gradingscale.py
@@ -0,0 +1,21 @@
+marks = int(input("Enter marks:"))
+if marks>=100:
+    print("Grade Error")
+elif marks>=80:
+    print("Grade A+")
+elif marks>=75:
+    print("Grade A")
+elif marks>=70:
+    print("Grade A-")
+elif marks>=65:
+    print("Grade B+")
+elif marks>=55:
+    print("Grade B-")
+elif marks>=50:
+    print("Grade C+")
+elif marks>=45:
+    print("Grade C")
+elif marks>=40:
+    print("Grade D")
+else:
+    print("fail")
